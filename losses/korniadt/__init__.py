141c24d132c1ca528d8ef97b3ceafaa91248d66d
CHANG Lufan <moyiliyi@users.noreply.github.com>
2024-05-29 10:42:57 +0800

    Update morphology

--- losses/korniadt/__init__.py
+++ losses/korniadt/__init__.py
@@ -2,4 +2,4 @@ from .distance_transform import distance_transform
 from .grid import create_meshgrid
 from .filter import filter2d
 from .kernels import normalize_kernel2d
-
+from .morphology import *
